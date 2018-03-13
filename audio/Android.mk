0120765d325246c13b2e426b7e0c19874d97abfa
Your Name <you@example.com>
2018-03-13 14:36:49 -0700

    tblte-common: Add support for tbltechn

--- audio/Android.mk
+++ audio/Android.mk
@@ -14,10 +14,10 @@ include $(BUILD_PREBUILT)
 
 else ifeq($(TARGET_DEVICE),tbltechn)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := mixer_paths_chn.xml
+LOCAL_MODULE       := mixer_paths.xml
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := mixer_paths_eur.xml
+LOCAL_SRC_FILES    := mixer_paths_chn.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 LOCAL_MODULE_RELATIVE_PATH := etc
 LOCAL_PROPRIETARY_MODULE      := true
