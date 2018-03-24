d66be438887836c2bf50660a280c06a079aa8fdf
Your Name <you@example.com>
2018-03-23 22:42:50 -0700

    fix audio

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_DEVICE),tbltexx,tbltechn)
+ifeq ($(TARGET_DEVICE),tbltexx)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := mixer_paths.xml
 LOCAL_MODULE_TAGS  := optional
@@ -10,6 +10,15 @@ LOCAL_SRC_FILES    := mixer_paths_eur.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 else
+ifeq ($(TARGET_DEVICE),tbltechn)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := mixer_paths.xml
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := mixer_paths_chn.xml
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
+endif
 include $(CLEAR_VARS)
 LOCAL_MODULE       := mixer_paths.xml
 LOCAL_MODULE_TAGS  := optional
